7400d639e15d249e90e0929b3dbed66f26e42b5c
Mohamed Hany Ahmed <mohamedharidy12123@gmail.com>
2025-12-10 14:20:28 +0200

    added sum

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
+int sum(int a, int b) {
+  return a + b;
+}
 int main() {
+  int a, b;
   cout << "Hello World" << endl;
 
   cout << "hola";
   cout << "chau";
+  cin >> a, b;
+  cout << sum(a, b) << endl;
   return 0;
 }
