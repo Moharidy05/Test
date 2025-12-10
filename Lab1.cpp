ddfb2fc0a5ffd667cde718f7eed847bf1ef7962e
Mohamed Hany Ahmed <mohamedharidy12123@gmail.com>
2025-12-10 14:37:14 +0200

    sum 3 params

--- Lab1.cpp
+++ Lab1.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-int sum(int a, int b) {
-  return a + b;
+
+int sum(int a, int b, int c) {
+  return a + b + c;
 }
+
 int main() {
   int a, b;
   cout << "Hello World" << endl;
