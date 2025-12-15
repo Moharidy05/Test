3e5b0bfb57893e1f3ec6b36bc7c8af6128222042
Moharidy05 <mohamedharidy12123@gmail.com>
2025-12-15 16:26:33 +0200

    added divide feature

--- Lab1.cpp
+++ Lab1.cpp
@@ -7,6 +7,13 @@ int sum(int a, int b, int c) {
   return a + b + c;
 }
 
+int divide(int a , int b){
+  if(b == 0){
+    cout << "Error: Division by zero" << endl;
+    return 0;
+  }
+  return a / b;
+}
 int main() {
   int a, b, c;
   cout << "Hello World" << endl;
